d5e7008848cb04d0bd78eb4d410375bca113d27c
Jason Rudinsky <jrudy710@gmail.com>
2024-09-01 09:46:11 -0400

    This will be the skeleton for how we will reverse the integer in the program.

--- ReverseInteger.java
+++ ReverseInteger.java
@@ -1,6 +1,6 @@
 /* Jason Rudinsky
 * September 1, 2024
-* Leetcode Question - Zigzag Conversion
+* Leetcode Question - Reverse Integer
 
 * This is a program that will take in the input of an integer and the program will
 * reverse the integer and return the number to the user. If the reversed integer is
